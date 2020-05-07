b4c15111abc96bb9e81db42b95afe9d14542454f
18302010060 黄怡清 <18302010060@fudan.edu.cn>
2020-05-07 12:58:07 +0800

    修改方法返回值，返回具体错误信息

--- src/main/java/fudan/se/lab2/service/AuthService.java
+++ src/main/java/fudan/se/lab2/service/AuthService.java
@@ -63,7 +63,7 @@ public class AuthService {
         return user;
     }*/
 
-    public Boolean register(RegisterRequest request) {
+    public String register(RegisterRequest request) {
         // TODO: Implement the function.
         //从request中获取用户注册信息的基本参数
         String username = request.getUsername();
@@ -91,15 +91,22 @@ public class AuthService {
         }*/
         //判断注册用户是否已经存在，若存在则进入用户已存在异常处理程序
         Optional<User> users = Optional.ofNullable(userRepository.findByUsername(username));
+        List<User> userList = userRepository.findAll();
         if(users.isPresent()) {
-            return false;
+            return "用户名已存在";
         }
+        for (User value : userList) {
+            if (email.equals(value.getEmail())) {
+                return "该邮箱已被注册过";
+            }
+        }
+
 
         //根据输入的五个参数以及authority构造USER新用户
         User user = new User(username, password, email, area, unit, fullname,new HashSet<>(Collections.singletonList(authority)));
         //在数据库中进行保存
         userRepository.save(user);
-        return true;
+        return "注册成功";
     }
 
 
@@ -109,17 +116,26 @@ public class AuthService {
         //在用户仓库中新建用户信息查询类
         JwtUserDetailsService jwtUserDetailsService = new JwtUserDetailsService(userRepository);
         //在类中调用重载方法，判断在库中是否存在该用户（若不存在则会在方法中进入异常处理程序）
+        try{
+            UserDetails user = jwtUserDetailsService.loadUserByUsername(username);
+            if(!password.equals(user.getPassword())){
+                return "密码错误";
+                //throw new WrongPasswordException();
 
-        UserDetails user = jwtUserDetailsService.loadUserByUsername(username);
-        if(!password.equals(user.getPassword())){
-            throw new WrongPasswordException();
-        }else {
+            }
 
-            JwtTokenUtil jwtTokenUtil=new JwtTokenUtil(new JwtConfigProperties());
-            String token=jwtTokenUtil.generateToken(userRepository.findByUsername(username));
+            else {
 
-            return token;
+                JwtTokenUtil jwtTokenUtil=new JwtTokenUtil(new JwtConfigProperties());
+                String token=jwtTokenUtil.generateToken(userRepository.findByUsername(username));
+
+                return token;
+            }
         }
+        catch(UsernameNotFoundException e){
+            return("用户名不存在");
+        }
+
 
 
     }
